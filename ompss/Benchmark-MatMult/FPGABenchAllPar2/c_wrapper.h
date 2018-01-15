276be971a229d6261d7d11e0fb53e4da216891f9
Lotfi Guedria <lotfi.guedria@cetic.be>
2018-01-15 23:41:20 +0100

    poroto/ompss second version of annotated testbench

---
+++ ompss/Benchmark-MatMult/FPGABenchAllPar2/c_wrapper.h
@@ -0,0 +1,1 @@
+extern void MatrixMultiplication(int **A, int **B, int height_A, int width_A_height_B, int width_B, int **C) ;
---
+++ ompss/Benchmark-MatMult/FPGABenchAllPar2/utils/timer.h
@@ -0,0 +1,17 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+
+void StartTimer();
+
+void ReStartTimer();
+
+void StopTimer();
+
+void GetTimer();
+
+void GetTimerStats(int n);
+
+void ResetTimerStats();
+
+#endif // TIMER_H
